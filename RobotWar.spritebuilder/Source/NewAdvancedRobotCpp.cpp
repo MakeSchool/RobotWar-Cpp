023aaa8cd2a410cb1c2c30e84f6549519822ccbb
Daniel Haaser <thedapperdan@gmail.com>
2015-07-06 11:22:18 -0700

    Fixed bug in AdvancedRobot

---
+++ RobotWar.spritebuilder/Source/NewAdvancedRobotCpp.cpp
@@ -0,0 +1,9 @@
+//
+//  NewAdvancedRobotCpp.cpp
+//  RobotWar
+//
+//  Created by Daniel Haaser on 7/6/15.
+//  Copyright (c) 2015 MakeGamesWithUs. All rights reserved.
+//
+
+#include "NewAdvancedRobotCpp.h"
---
+++ RobotWar.spritebuilder/Source/NewAdvancedRobotCpp.h
@@ -0,0 +1,14 @@
+//
+//  NewAdvancedRobotCpp.h
+//  RobotWar
+//
+//  Created by Daniel Haaser on 7/6/15.
+//  Copyright (c) 2015 MakeGamesWithUs. All rights reserved.
+//
+
+#ifndef __RobotWar__NewAdvancedRobotCpp__
+#define __RobotWar__NewAdvancedRobotCpp__
+
+#include <stdio.h>
+
+#endif /* defined(__RobotWar__NewAdvancedRobotCpp__) */
