fce2758314c29af9d5400fb1d65b86848f297611
eiji-ito <eiji.ito@gree.co.jp>
2016-05-06 18:31:22 +0900

    add new robot

---
+++ RobotWar.spritebuilder/EijiRobotCpp.cpp
@@ -0,0 +1,9 @@
+//
+//  EijiRobotCpp.cpp
+//  RobotWar
+//
+//  Created by Eiji Ito on 2016/05/06.
+//  Copyright © 2016年 MakeGamesWithUs. All rights reserved.
+//
+
+#include "EijiRobotCpp.hpp"
---
+++ RobotWar.spritebuilder/EijiRobotCpp.hpp
@@ -0,0 +1,14 @@
+//
+//  EijiRobotCpp.hpp
+//  RobotWar
+//
+//  Created by Eiji Ito on 2016/05/06.
+//  Copyright © 2016年 MakeGamesWithUs. All rights reserved.
+//
+
+#ifndef EijiRobotCpp_hpp
+#define EijiRobotCpp_hpp
+
+#include <stdio.h>
+
+#endif /* EijiRobotCpp_hpp */
