7871a6c039b0dfe96a3984286581c0a13afd4ca3
Shuhei Asada <shuhei.asada@g-pc-4486.intra.gree-office.net>
2016-05-09 11:42:17 +0900

    modify AsadaRobot

--- RobotWar.spritebuilder/Source/AsadaRobotCpp.cpp
+++ RobotWar.spritebuilder/Source/AsadaRobotCpp.cpp
@@ -8,10 +8,13 @@
 
 #include "AsadaRobotCpp.hpp"
 #include <math.h>
+#include <stdio.h>
 
 void AsadaRobotCpp::run()
 {
     init();
+    
+    this->turnGunLeft(30);
     
     while (true)
     {
@@ -23,12 +26,12 @@ void AsadaRobotCpp::run()
             {
                 switch (this->m_StateFrame)
                 {
-                    case 0 :
-                    {
-                      
-                    }
-                    break;
-                    case 1 : this->moveBack(999999); break;
+                    //case 0 :
+                    //{
+                    //
+                    //}
+                    //break;
+                    case 0 : this->moveBack(999999); break;
                 }
                 break;
             }
@@ -36,14 +39,37 @@ void AsadaRobotCpp::run()
             case STATE_AHEAD:
                 switch (this->m_StateFrame)
                 {
-                    case 0 : this->moveAhead(999999); break;
+                    case 0 :
+                    {
+                        float angle = this->angleBetweenGunHeadingDirectionAndWorldPosition(this->gunHeadingDirection()) - this->angleBetweenHeadingDirectionAndWorldPosition(this->headingDirection());
+                        
+                        printf( "%lf¥r¥n", angle );
+                        
+                        if (angle > 29)
+                        {
+                            int anglen = fabs(angle) - 30;
+                            if( anglen > 0 ){
+                                this->turnGunRight( anglen );
+                            }
+                        }
+                        else
+                        {
+                            int anglen = 30;
+                            if( anglen > 0 ){
+                                this->turnGunLeft( anglen );
+                            }
+                        }
+                        
+                        break;
+                    }
+                    case 1 : this->moveAhead(999999); break;
                 }
                 break;
                 
             case STATE_TURNRIGHT90:
                 switch (this->m_StateFrame)
                 {
-                    case 0 :  this->moveAhead(80); break;
+                    case 0 :  this->moveAhead(15); break;
                     case 1 :  this->turnRobotRight(90); break;
                     case 2 :  if( this->m_Next == STATE_NONE ){this->m_Next = STATE_AHEAD;} break;
                 }
@@ -52,7 +78,7 @@ void AsadaRobotCpp::run()
             case STATE_TURNLEFT90:
                 switch (this->m_StateFrame)
                 {
-                    case 0 :  this->moveBack(80); break;
+                    case 0 :  this->moveBack(15); break;
                     case 1 :  this->turnRobotLeft(90); break;
                     case 2 :  if( this->m_Next == STATE_NONE ){this->m_Next= STATE_AHEAD;} break;
                 }
